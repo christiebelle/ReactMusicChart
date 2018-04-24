8747e9b0466bf81250272bafc2614b5e0c3d6d11
Christiebelle <cslester@outlook.com>
2018-04-24 19:44:16 +0100

    render method added logging out hello for testing

---
+++ src/containers/ChartDisplay.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class ChartDisplay extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      chartsongs: [],
+      currentSong: null
+    };
+  }
+
+  render(){
+    return(
+      console.log("hello!");
+    )
+  }
+
+};
+
+export default ChartDisplay;
